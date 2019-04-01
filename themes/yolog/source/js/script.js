96fb8e5c55dacff91a8137cebb5109eca8e6245d
辛跃飞 <yuefei.xin@yiducloud.cn>
2019-04-01 16:41:32 +0800

    openIframe

--- themes/yolog/source/js/script.js
+++ themes/yolog/source/js/script.js
@@ -1,38 +1,38 @@
-function addEventListener(el, eventName, handler) {
-	if (el.addEventListener) {
-		el.addEventListener(eventName, handler);
-	} else {
-		el.attachEvent('on' + eventName, function() {
-			handler.call(el);
-		});
-	}
-}
-
-function toggleClass(el, className) {
-	if (el.classList) {
-		el.classList.toggle(className);
-	} else {
-		var classes = el.className.split(' ');
-		var existingIndex = -1;
-		for (var i = classes.length; i--;) {
-			if (classes[i] === className) existingIndex = i;
-		}
-		if (existingIndex >= 0) {
-			classes.splice(existingIndex, 1);
-		} else {
-			classes.push(className);
-		}
-		el.className = classes.join(' ');
-	}
-}
-
-var btn = document.querySelector('.toggle-menu-btn');
-var header = document.querySelector('header');
-addEventListener(btn, 'click', function() {
-	toggleClass(header, 'open');
+document.querySelector('.toggle-menu-btn').addEventListener('click', function() {
+	document.querySelector('header').classList.toggle('open');
 });
 
 var aboutNode = document.createElement('li');
 aboutNode.className = 'category-list-item';
 aboutNode.innerHTML = '<a class="category-list-link" href="/uncategorized/about.html">About</a>';
 document.querySelector('.category-list').appendChild(aboutNode);
+
+
+function openIframe(url) {
+	var body = document.body;
+	var wrapper = document.createElement('div');
+	wrapper.className = 'iframe-wrapper';
+
+	var iframe = document.createElement('iframe');
+	iframe.setAttribute('frameborder', '0');
+	iframe.src = url;
+	iframe.onload = function() {
+		wrapper.classList.add('open');
+	};
+
+	var btn = document.createElement('button');
+	btn.className = 'iframe-btn';
+
+	wrapper.appendChild(iframe);
+	wrapper.appendChild(btn);
+	body.classList.add('over-hidden');
+	body.appendChild(wrapper);
+
+	function remove() {
+		btn.removeEventListener('click', remove);
+		body.removeChild(wrapper);
+		body.classList.remove('over-hidden');
+	}
+
+	btn.addEventListener('click', remove);
+}
